d26964b17d81987ea415c493ec521e3082946946
Jose Armesto <github@armesto.net>
2022-01-04 16:40:59 +0100

    Remove unnecesary manifests

--- Makefile.gen.app.mk
+++ Makefile.gen.app.mk
@@ -19,4 +19,4 @@ lint-chart: ## Runs ct against the default chart.
 .PHONY: generate-helm
 generate-helm:
 	kustomize build config/helm -o helm/cluster-api-provider-azure/templates
-	rm -rf helm/cluster-api-provider-azure/templates/apiextensions*
+	rm -rf helm/cluster-api-provider-azure/templates/apiextensions* helm/cluster-api-provider-azure/templates/apps_v1_daemonset* helm/cluster-api-provider-azure/templates/cert-manager.io_v1_issuer* helm/cluster-api-provider-azure/templates/*aad-pod-id-nmi*
