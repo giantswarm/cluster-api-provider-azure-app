414c9b07c78072abb13262288023c2177578b0fc
Jose Armesto <github@armesto.net>
2022-01-11 11:59:17 +0100

    Use different Makefile

--- Makefile.gen.app.mk
+++ Makefile.gen.app.mk
@@ -15,8 +15,3 @@ lint-chart: ## Runs ct against the default chart.
 	architect helm template --dir /tmp/$(APPLICATION)-test/helm/$(APPLICATION)
 	docker run -it --rm -v /tmp/$(APPLICATION)-test:/wd --workdir=/wd --name ct $(IMAGE) ct lint --validate-maintainers=false --charts="helm/$(APPLICATION)"
 	rm -rf /tmp/$(APPLICATION)-test
-
-.PHONY: generate-helm
-generate-helm:
-	kustomize build config/helm -o helm/cluster-api-provider-azure/templates
-	rm -rf helm/cluster-api-provider-azure/templates/apiextensions* helm/cluster-api-provider-azure/templates/apps_v1_daemonset* helm/cluster-api-provider-azure/templates/cert-manager.io_v1_issuer* helm/cluster-api-provider-azure/templates/*aad-pod-id-nmi* helm/cluster-api-provider-azure/templates/v1_secret_capz-manager-bootstrap-credentials.yaml
