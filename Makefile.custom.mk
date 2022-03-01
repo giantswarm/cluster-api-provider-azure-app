aed74505d987c555eb991e634b0d53ff37bca821
Jose Armesto <github@armesto.net>
2022-03-01 09:34:27 +0100

    Kustomize improvements (#47)
    * Remove leader election
    
    * Add sso secret as a kustomize resource
    
    * Delete unnecesary manifests
    
    * Add verify CI action
    
    * Update changelog
    
    * Delete output when running make command

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -1,4 +1,12 @@
 .PHONY: generate
 generate:
-	kustomize build config/helm -o helm/cluster-api-provider-azure/templates
-	rm -rf helm/cluster-api-provider-azure/templates/apiextensions* helm/cluster-api-provider-azure/templates/apps_v1_daemonset* helm/cluster-api-provider-azure/templates/cert-manager.io_v1_issuer* helm/cluster-api-provider-azure/templates/*aad-pod-id-nmi* helm/cluster-api-provider-azure/templates/v1_secret_capz-manager-bootstrap-credentials.yaml
+	@rm -rf helm/cluster-api-provider-azure/templates/*.yaml
+	@kustomize build config/helm -o helm/cluster-api-provider-azure/templates
+	@rm -rf helm/cluster-api-provider-azure/templates/apiextensions*.yaml helm/cluster-api-provider-azure/templates/v1_secret_capz-manager-bootstrap-credentials.yaml
+
+.PHONY: verify
+verify: generate
+	@if !(git diff --quiet HEAD); then \
+		git diff; \
+		echo "generated files are out of date, run make generate"; exit 1; \
+	fi
