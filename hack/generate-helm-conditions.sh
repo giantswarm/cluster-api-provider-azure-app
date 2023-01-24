029c1536dc16e75a7de36fc2aecb9767f1817db8
Francesco Ciocchetti <2085772+primeroz@users.noreply.github.com>
2023-01-24 09:13:36 +0100

    Remove NMI so i can test using our own AAD pod identity app for CAPZ (#87)
    * Remove NMI so i can test using our own AAD pod identity app for CAPZ
    
    * Remove AAD CRDs
    
    * Restore NMI RBAC which we still need even when running NMI from another APP
    
    * Update Changelog

--- hack/generate-helm-conditions.sh
+++ hack/generate-helm-conditions.sh
@@ -12,12 +12,8 @@ capi_only_templates=()
 capi_only_templates+=("${HELM_DIR}/templates/v1_secret_credential-default.yaml")
 capi_only_templates+=("${HELM_DIR}/templates/rbac.authorization.k8s.io_v1_role_capz-leader-election-role.yaml")
 capi_only_templates+=("${HELM_DIR}/templates/rbac.authorization.k8s.io_v1_rolebinding_capz-leader-election-rolebinding.yaml")
-capi_only_templates+=("${HELM_DIR}/templates/apps_v1_daemonset_capz-nmi.yaml")
 capi_only_templates+=("${HELM_DIR}/templates/rbac.authorization.k8s.io_v1_clusterrole_capz-aad-pod-id-nmi-role.yaml")
 capi_only_templates+=("${HELM_DIR}/templates/rbac.authorization.k8s.io_v1_clusterrolebinding_capz-aad-pod-id-nmi-binding.yaml")
-capi_only_templates+=("${HELM_DIR}/templates/apiextensions.k8s.io_v1_customresourcedefinition_azureidentities.aadpodidentity.k8s.io.yaml")
-capi_only_templates+=("${HELM_DIR}/templates/apiextensions.k8s.io_v1_customresourcedefinition_azureidentitybindings.aadpodidentity.k8s.io.yaml")
-capi_only_templates+=("${HELM_DIR}/templates/apiextensions.k8s.io_v1_customresourcedefinition_azurepodidentityexceptions.aadpodidentity.k8s.io.yaml")
 
 for template_file in "${capi_only_templates[@]}"; do
     sed -i '1i {{ if eq .Values.provider.flavor "capi" }}' "$template_file"
