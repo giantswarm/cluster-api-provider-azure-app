1d13e219bd624d57e76a38bbb723d87575a81a83
Marco Ebert <marco_ebert@icloud.com>
2025-04-29 13:57:18 +0200

    Repository: Some chores. (#180)
    * CircleCI: Rework `config.yml`.
    
    * GitHub: Rework `verify.yaml`.
    
    * Repository: Clean `.gitignore`.
    
    * Repository: Remove `.nancy-ignore`.
    
    * Makefile: Use `kubectl kustomize`.
    
    * Repository: Rework `README.md`.

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -3,15 +3,8 @@ generate:
 	./hack/generate-kustomize-patches.sh
 	@rm -rf helm/cluster-api-provider-azure/templates/*.yaml
 	@cp config/helm/copy/* helm/cluster-api-provider-azure/templates/
-	@kustomize build config/helm -o helm/cluster-api-provider-azure/templates
+	@kubectl kustomize config/helm --output helm/cluster-api-provider-azure/templates
 	./hack/move-generated-crds.sh
 	./hack/generate-crd-version-patches.sh
 	./hack/cleanup-helm-templates.sh
 	./hack/generate-helm-conditions.sh
-
-.PHONY: verify
-verify: generate
-	@if !(git diff --quiet HEAD); then \
-		git diff; \
-		echo "generated files are out of date, run make generate"; exit 1; \
-	fi
