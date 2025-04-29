a3e93033999dfc889e46e3530e80bace6cb69099
Marco Ebert <marco_ebert@icloud.com>
2025-04-29 13:06:51 +0200

    Makefile: Use `kubectl kustomize`.

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -3,7 +3,7 @@ generate:
 	./hack/generate-kustomize-patches.sh
 	@rm -rf helm/cluster-api-provider-azure/templates/*.yaml
 	@cp config/helm/copy/* helm/cluster-api-provider-azure/templates/
-	@kustomize build config/helm -o helm/cluster-api-provider-azure/templates
+	@kubectl kustomize config/helm --output helm/cluster-api-provider-azure/templates
 	./hack/move-generated-crds.sh
 	./hack/generate-crd-version-patches.sh
 	./hack/cleanup-helm-templates.sh
