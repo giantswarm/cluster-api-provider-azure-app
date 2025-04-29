ddb9c982791b9da6e6efb639996b635c3d98e6f7
Marco Ebert <marco_ebert@icloud.com>
2025-04-29 12:44:35 +0200

    GitHub: Rework `verify.yaml`.

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -8,10 +8,3 @@ generate:
 	./hack/generate-crd-version-patches.sh
 	./hack/cleanup-helm-templates.sh
 	./hack/generate-helm-conditions.sh
-
-.PHONY: verify
-verify: generate
-	@if !(git diff --quiet HEAD); then \
-		git diff; \
-		echo "generated files are out of date, run make generate"; exit 1; \
-	fi
