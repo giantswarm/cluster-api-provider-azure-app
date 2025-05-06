2cf371bbb8f4bb06a3ea5f978558494de89dfb34
Marco Ebert <marco_ebert@icloud.com>
2025-05-06 15:44:11 +0200

    Hack: Rework `move-generated-crds.sh` into `move-crds.sh`.

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -10,7 +10,9 @@ generate:
 	rm -f helm/cluster-api-provider-azure/templates/*.yaml
 	kubectl kustomize config/helm --output helm/cluster-api-provider-azure/templates
 
-	./hack/move-generated-crds.sh
+	# Move CRDs.
+	hack/move-crds.sh
+
 	./hack/generate-crd-version-patches.sh
 	./hack/cleanup-helm-templates.sh
 	./hack/generate-helm-conditions.sh
