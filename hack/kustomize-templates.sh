2ede383603524cab2684a66c35d8a49288892e0d
Robin Ketelbuters <robin.k@giantswarm.io>
2026-09-18 12:16:03 +0200

    fix: Replace `clusterctl`-style environment substition with Kubernetes-style (#266)

--- hack/kustomize-templates.sh
+++ hack/kustomize-templates.sh
@@ -9,4 +9,9 @@ echo "using kubectl version $(echo $version | jq -r .clientVersion.gitVersion)"
 echo "using bundled kustomize version $(echo $version | jq -r .kustomizeVersion)"
 
 rm -f helm/cluster-api-provider-azure/templates/*.yaml
+
+# Replace `clusterctl` variable expansion "${ADDITIONAL_ASO_CRDS}" with "$(ADDITIONAL_ASO_CRDS)",
+# which works at runtime for reading a container env var.
+sed -i -E 's|\$\{ADDITIONAL_ASO_CRDS(:[=-][^}]*)?\}|$(ADDITIONAL_ASO_CRDS)|g' config/helm/bases/infrastructure-components.yaml
+
 kubectl kustomize config/helm --output helm/cluster-api-provider-azure/templates
